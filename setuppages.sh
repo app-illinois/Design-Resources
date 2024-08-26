a7253dc32edf800459b85d1ffae6ae6ccdd4b681
nathanb-UIUC <68081473+nathanb-UIUC@users.noreply.github.com>
2024-08-26 14:53:09 -0500

    Update setuppages.sh

--- setuppages.sh
+++ setuppages.sh
@@ -1,4 +1,6 @@
 #cp ./applications/buttons/*.css ./docs/css 
 #cp ./applications/buttons/*.js ./docs/js
+mkdir -p ./docs/css
+mkdir -p ./docs/js
 find ./applications -name *.js -exec cp {} ./docs/js \;
 find ./applications -name *.css -exec cp {} ./docs/css \;
