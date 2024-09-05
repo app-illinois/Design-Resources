001dc0c9208199aae1bc5e7758fbf1fa8a1a85be
Jill Fleming <fleming7@illinois.edu>
2024-09-05 12:25:47 -0500

    made it so toasts stack in order that they are opened

--- applications/ila-toast/ila-toast.js
+++ applications/ila-toast/ila-toast.js
@@ -2,6 +2,13 @@ function openToast(toastId) {
     let toast = document.getElementById(toastId);
     toast.classList.remove('ila-toast--closed');
     toast.classList.add('ila-toast--open');
+
+    // remove property from older elements so newest element appears on bottom
+    const olderElements = document.querySelectorAll('.ila-toast--first');
+    olderElements.forEach((element) => {
+        element.classList.remove('ila-toast--first');
+    });
+    toast.classList.add('ila-toast--first');
 }
 
 function closeToast(toastId) {
