f20c6416da4de6b6d48b741a5b71d5f86228329e
Bryan Jonker <jonker@illinois.edu>
2024-11-27 11:15:42 -0600

    Adding app-toolkit.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,26 @@ const gulp = require('gulp');
 const { series } = require('gulp');
 const cleanCSS = require('gulp-clean-css');
 const concatCss = require('gulp-concat-css');
+const concat = require('gulp-concat');
+const terser = require('gulp-terser');
+
+function concatJS(){
+  return gulp.src('./applications/**/*.js')
+    .pipe(concat('app-toolkit.js'))
+    .pipe(gulp.dest('./dist'));
+}
+
+function copySVG(){
+  return gulp.src('./applications/**/*.svg')
+    .pipe(gulp.dest('docs/svg/'));
+}
+
+function minifyJS(){
+  return gulp.src('./dist/app-toolkit.js')
+    .pipe(terser())
+    .pipe(gulp.dest('docs/js/'));
+}
+
 
 function concatCSS() {
     return gulp.src('applications/**/*.css')
@@ -16,4 +36,4 @@ function concatCSS() {
   }
   
   
-exports.default = series(concatCSS, minifyCSS);
+exports.default = series(concatCSS, minifyCSS, concatJS, minifyJS, copySVG);
