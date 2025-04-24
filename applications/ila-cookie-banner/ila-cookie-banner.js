ed3ba3e5e7058b609e900a53b10b457160b434ec
Michelle Pitcel <mpitcel2@illinois.edu>
2025-04-24 13:41:11 -0500

    Modal-type updates: - Add new `div` for modal-type shading behind the Cookie Banner when open - Add JS functions to add and remove modal-type shading and enable/disable scrolling - Add various CSS settings to work with new functionality

--- applications/ila-cookie-banner/ila-cookie-banner.js
+++ applications/ila-cookie-banner/ila-cookie-banner.js
@@ -1,4 +1,4 @@
-function openCookieB(cookiebId, focusOnLoad) {
+function openCookieB(cookiebId, focusOnLoad, modalID) {
     let cookieb = document.getElementById(cookiebId);
     cookieb.classList.remove('ila-cookieb--closed');
     cookieb.classList.add('ila-cookieb--open');
@@ -10,14 +10,24 @@ function openCookieB(cookiebId, focusOnLoad) {
     });
     cookieb.classList.add('ila-cookieb--first');
     manageAutoclose(cookiebId);
-    
+
+    document.body.classList.add('ila-cookieb-noscroll');
+
+    let modalIDvar = document.getElementById(modalID);
+    modalIDvar.classList.add('ila-cookieb-modal');
+
     document.getElementById(focusOnLoad).focus();
 }
 
-function closeCookieB(cookiebId) {
+function closeCookieB(cookiebId, modalID) {
     let cookieb = document.getElementById(cookiebId);
     cookieb.classList.remove('ila-cookieb--open');
     cookieb.classList.add('ila-cookieb--closed');
+
+    document.body.classList.remove('ila-cookieb-noscroll');
+
+    let modalIDvar = document.getElementById(modalID);
+    modalIDvar.classList.remove('ila-cookieb-modal');
 }
 
 function manageAutoclose(cookiebId) {
