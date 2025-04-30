47d297bd742089270bb8b06fd4274d5d203efea6
mpitcel <3997366+mpitcel@users.noreply.github.com>
2025-04-30 10:25:37 -0500

    Various Updates for Issue 133 (#134)
    Closes #133 
    
    Updates: 
    
    - Slideout wider on smaller screens 
    - Add space above Banner Heading 
    - Make Details in Slideout change pointer for clickable area on hover 
    - Add Close button to the bottom of the Slideout 
    - Hide Cookie Banner and Slideout when printing 
    - Mirror changes between the two HTML files 
    - Add missing `type` to `button` tags 
    - Hover and Focus both change button appearance 
    - Added popup to links in the Cookie Banner and the About Slideout 
    - Added `active` options to the CSS in addition to `hover` and `focus` 
    - Various reformatting for readability 
    - Focus on the Cookie Banner on Load 
    
    ---------
    
    Co-authored-by: David Riddle <ddriddle@illinois.edu>
    Co-authored-by: Edward Delaporte <delaport@illinois.edu>
    Co-authored-by: Michelle Pitcel <mpitcel2@illinois.edu>
    Co-authored-by: Tyler Turner <tzturner@illinois.edu>
    Co-authored-by: Zach Carrington <zdc@illinois.edu>

--- applications/ila-cookie-banner/ila-cookie-banner.js
+++ applications/ila-cookie-banner/ila-cookie-banner.js
@@ -1,4 +1,4 @@
-function openCookieB(cookiebId) {
+function openCookieB(cookiebId, focusOnLoad) {
     let cookieb = document.getElementById(cookiebId);
     cookieb.classList.remove('ila-cookieb--closed');
     cookieb.classList.add('ila-cookieb--open');
@@ -10,12 +10,28 @@ function openCookieB(cookiebId) {
     });
     cookieb.classList.add('ila-cookieb--first');
     manageAutoclose(cookiebId);
+
+    // Used to disable scroll on the page 
+    // document.body.classList.add('ila-cookieb-noscroll');
+
+    // Used to enable a modal background on the page
+    // let modalIDvar = document.getElementById(modalID);
+    // modalIDvar.classList.add('ila-cookieb-modal');
+
+    document.getElementById(focusOnLoad).focus();
 }
 
 function closeCookieB(cookiebId) {
     let cookieb = document.getElementById(cookiebId);
     cookieb.classList.remove('ila-cookieb--open');
     cookieb.classList.add('ila-cookieb--closed');
+
+    // Used to enable scroll on the page 
+    // document.body.classList.remove('ila-cookieb-noscroll');
+
+    // Used to disable a modal background on the page
+    // let modalIDvar = document.getElementById(modalID);
+    // modalIDvar.classList.remove('ila-cookieb-modal');
 }
 
 function manageAutoclose(cookiebId) {
