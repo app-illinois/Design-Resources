ccddd9f9b9e72ca1b0adc4b0cacd6a11dbd54efe
Edward Delaporte <delaport@illinois.edu>
2025-05-02 16:51:36 -0500

    Swap debug paths for preferred.

--- applications/ila-cookie-banner/ila-cookie-banner.js
+++ applications/ila-cookie-banner/ila-cookie-banner.js
@@ -44,7 +44,7 @@ async function addCookieB(cookiebId) {
     /* Preferred path */
     var content_path = 'js/ila-cookie-banner-content.html';
     /* Path For Debugging */ 
-    var content_path = '/applications/ila-cookie-banner/ila-cookie-banner-content.html';
+    // var content_path = '/applications/ila-cookie-banner/ila-cookie-banner-content.html';
     let banner_response = await fetch(content_path);
     let banner_content = await banner_response.text();
     cookieb.insertAdjacentHTML("afterbegin", banner_content);
