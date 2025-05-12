dcedc2af8579ced025852bbf7c17dee978754501
David D. Riddle <ddriddle@illinois.edu>
2025-05-12 15:44:30 -0500

    Add data attribute for switching themes

--- applications/ila-cookie-banner/ila-cookie-banner.js
+++ applications/ila-cookie-banner/ila-cookie-banner.js
@@ -48,14 +48,25 @@ async function getCookieBannerContent(content_path) {
 
 async function addCookieBanner() {
     /* Appends to the end of the page. */
+    // Decide which theme to use
+    let theme = document.getElementById("cookie-banner-script").getAttribute("data-theme");
+    switch(theme) {
+        case "uic":
+            css_content = await getCookieBannerContent('css/ila-cookie-uic-colors.css');
+            break;
+        case "uis":
+            css_content = await getCookieBannerContent('css/ila-cookie-uis-colors.css');
+            break;
+        default:
+            css_content = await getCookieBannerContent('css/ila-cookie-uiuc-colors.css');
+            break;
+    }
     
     // Add cookie banner to page
     let banner_content = "<style>";
     banner_content += await getCookieBannerContent('css/ila-slideover.css');
-    banner_content += await getCookieBannerContent('css/ila-cookie-uiuc-colors.css');
-    // banner_content += await getCookieBannerContent('css/ila-cookie-uic-colors.css');
-    // banner_content += await getCookieBannerContent('css/ila-cookie-uis-colors.css');
     banner_content += await getCookieBannerContent('css/ila-cookie-banner.css');
+    banner_content += css_content;
     banner_content += "</style>";
     banner_content += await getCookieBannerContent('partials/ila-cookie-banner-content.part.html');
     document.body.insertAdjacentHTML("beforeend", banner_content);
