8c01a09247e13549487d4994fb041ae347fab2a2
Tyler Turner <tzturner@illinois.edu>
2025-05-27 15:47:43 -0500

    Add make-cookie-zip.sh and cookie-banner-zip.yaml

---
+++ make-cookie-zip.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+mkdir -p ./cookie-zip-stage
+mkdir -p ./cookie-zip-stage/js
+mkdir -p ./cookie-zip-stage/css
+mkdir -p ./cookie-zip-stage/partials
+
+# Start staging
+cp ./applications/ila-cookie-banner/*.js ./cookie-zip-stage/js
+cp ./applications/ila-cookie-banner/*.css ./cookie-zip-stage/css
+cp ./applications/ila-cookie-banner/*.part.html ./cookie-zip-stage/partials
+
+# Pack slideover in with cookie banner
+cat applications/ila-slideovers/ila-slideover.js >> ./cookie-zip-stage/js/ila-cookie-banner.js
+
+# Leave a copy under the old name for old hardcoded URLs
+cp ./cookie-zip-stage/js/ila-cookie-banner.js ../cookie-zip-stage/otSDKStub.js
