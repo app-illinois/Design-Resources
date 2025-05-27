7a7a6230724847b6cf5b169e74c065c04204d673
Edward Delaporte <delaport@illinois.edu>
2025-05-27 16:09:31 -0500

    FIXUP

--- make-cookie-zip.sh
+++ make-cookie-zip.sh
@@ -10,7 +10,7 @@ cp ./applications/ila-cookie-banner/*.css ./cookie-zip-stage/css
 cp ./applications/ila-cookie-banner/*.part.html ./cookie-zip-stage/partials
 
 # Pack slideover in with cookie banner
-cat applications/ila-slideovers/ila-slideover.js >> ./cookie-zip-stage/js/ila-cookie-banner.js
+cat applications/ila-slideovers/ila-slideover.js >>./cookie-zip-stage/js/ila-cookie-banner.js
 
 # Leave a copy under the old name for old hardcoded URLs
-cp ./cookie-zip-stage/js/ila-cookie-banner.js ../cookie-zip-stage/otSDKStub.js
+cp ./cookie-zip-stage/js/ila-cookie-banner.js ./cookie-zip-stage/otSDKStub.js
