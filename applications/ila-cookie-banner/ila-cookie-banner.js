8f3e04effb356f70ca1207a56f7b0111bf424e2a
Edward Delaporte <delaport@illinois.edu>
2025-06-06 14:55:35 -0500

    Fixup

--- applications/ila-cookie-banner/ila-cookie-banner.js
+++ applications/ila-cookie-banner/ila-cookie-banner.js
@@ -2,10 +2,10 @@
 // let cookie_banner_root_url = 'https://onetrust.techservices.illinois.edu/1.0.0';
 
 // For demonstrating the latest version on the demo site:
-// let cookie_banner_root_url = 'https://app-illinois.github.io/Design-Resources';
+let cookie_banner_root_url = 'https://app-illinois.github.io/Design-Resources';
 
 // For local testing purposes, uncomment the line below and comment out the line above:
-let cookie_banner_root_url = '.';
+// let cookie_banner_root_url = '.';
 let get_url = cookie_banner_root_url;
 
 var this_script = document.currentScript; // Must run before any function calls
