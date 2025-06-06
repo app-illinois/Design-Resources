30f492633e692d07134c2ba74139b99a060be6e4
Edward Delaporte <delaport@illinois.edu>
2025-06-06 14:49:42 -0500

    Set initial slide-over focus to slide-over dialog with tabindex -1

--- applications/ila-slideovers/ila-slideover.js
+++ applications/ila-slideovers/ila-slideover.js
@@ -141,9 +141,10 @@ aria.Dialog = function (dialogId, returnFocus) {
     );
     this.postNode.tabIndex = 0;
 
-    let title_tag = document.querySelector(`.ila-slideover__label`);
-    title_tag.setAttribute('tabindex', '0');
-    setTimeout(function(){    title_tag.focus();   },500);
+    // Set the initial focus for screen-readers
+    let slide_div = document.getElementById(dialogId);
+    slide_div.setAttribute('tabindex', '-1'); // Focusable, but outside tab order
+    setTimeout(function(){    slide_div.focus();   },500);
 
     this.addListeners();
     aria.openedDialog = this;
